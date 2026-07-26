12b388538984b27ab8c78c580d7becdb12c882ba
Ivan Joseph Basibas <ivanjosephbasibas@gmail.com>
2026-07-26 17:10:04 +0800

    Modal box contents and inputs then render it on the UI with updated dashboar

--- script.js
+++ script.js
@@ -32,45 +32,26 @@ addExpenseButton.addEventListener("click", function (e) {
 
 const overlay = document.querySelector(".modal-overlay");
 
-const overlayCheck = function () {
+const hideModal = function () {
+  document.querySelector(".modal-overlay").classList.add("hidden");
+  document.body.classList.remove("no-scroll");
+};
+
+const closeModal = function () {
   if (!overlay) return;
   overlay.addEventListener("click", function (e) {
     if (e.target.classList.contains("modal-overlay")) {
-      document.querySelector(".modal-overlay").classList.add("hidden");
-      document.body.classList.remove("no-scroll");
+      hideModal();
     }
   });
 };
 
-overlayCheck();
+closeModal();
 
 const expenses = [
   {
+    icon: 0,
     amount: -200,
-    category: "Food and drinks",
-    description: "Jollibee",
-    date: "June 9, 1994",
-  },
-  {
-    amount: -500,
-    category: "Food and drinks",
-    description: "Jollibee",
-    date: "June 9, 1994",
-  },
-  {
-    amount: -800,
-    category: "Food and drinks",
-    description: "Jollibee",
-    date: "June 9, 1994",
-  },
-  {
-    amount: -1100,
-    category: "Food and drinks",
-    description: "Jollibee",
-    date: "June 9, 1994",
-  },
-  {
-    amount: -600,
     category: "Food and drinks",
     description: "Jollibee",
     date: "June 9, 1994",
@@ -84,7 +65,6 @@ const totalExpense = function () {
 
 //Expense  figure
 const expenseAmountEl = document.querySelector(".total-figure");
-expenseAmountEl.textContent = `₱${totalExpense().toFixed(2, 0)}`;
 
 //Total Income
 const totalIncome = 6000;
@@ -94,28 +74,49 @@ const incomeEl = document.querySelector(".income-figure");
 incomeEl.textContent = `₱${totalIncome.toFixed(2, 0)}`;
 
 //Budget Left computation
-const budgetLeft = function (totalIncome) {
+const budgetLeft = function () {
   return totalIncome - totalExpense();
 };
 
 //Budget figure
 const budgetLeftEl = document.querySelector(".budget-figure");
-budgetLeftEl.textContent = `₱${budgetLeft(totalIncome).toFixed(2, 0)}`;
 
 //Savings Rate
 const savingsRate = function () {
-  return Math.floor((budgetLeft(totalIncome) / totalIncome) * 100);
+  return Math.floor((budgetLeft() / totalIncome) * 100);
 };
 
 //Saving figure
 const savingEl = document.querySelector(".savings-figure");
-savingEl.textContent = `${savingsRate()}%`;
 
+//UPDATE DASHBOARD
+const updateDashboard = function () {
+  expenseAmountEl.textContent = `₱${totalExpense().toFixed(2, 0)}`;
+  budgetLeftEl.textContent = `₱${budgetLeft(totalIncome).toFixed(2, 0)}`;
+  savingEl.textContent = `${savingsRate()}%`;
+};
+
+updateDashboard();
+const tableBody = document.querySelector("tbody");
 const renderExpense = function () {
   expenses.forEach((expense) => {
     const html = `<tr class="expense-body-column">
                   <td>
-                    <svg
+                    ${expenseIcons[expense.icon]}
+                    <span>${expense.category}</span>
+                  </td>
+                  <td>${expense.description}</td>
+                  <td>${expense.date}</td>
+                  <td>${Math.abs(expense.amount)}</</td>
+                </tr>
+                `;
+
+    tableBody.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+const expenseIcons = [
+  `<svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -128,17 +129,82 @@ const renderExpense = function () {
                         stroke-linejoin="round"
                         d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z"
                       />
-                    </svg>
-                    <span>${expense.category}</span>
-                  </td>
-                  <td>${expense.description}</td>
-                  <td>${expense.date}</</td>
-                  <td>${Math.abs(expense.amount)}</</td>
-                </tr>
-                `;
-
-    document.querySelector("tbody").insertAdjacentHTML("beforeend", html);
-  });
-};
+  </svg>`,
+  `<svg 
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-table icon-positive">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+</svg>`,
+  `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-table icon-yellow">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+</svg>
+`,
+  `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-table icon-neutral">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+</svg>
+`,
+  `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-table icon-negative">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z" />
+</svg>
+`,
+  `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-table icon-positive">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3.75v16.5M2.25 12h19.5M6.375 17.25a4.875 4.875 0 0 0 4.875-4.875V12m6.375 5.25a4.875 4.875 0 0 1-4.875-4.875V12m-9 8.25h16.5a1.5 1.5 0 0 0 1.5-1.5V5.25a1.5 1.5 0 0 0-1.5-1.5H3.75a1.5 1.5 0 0 0-1.5 1.5v13.5a1.5 1.5 0 0 0 1.5 1.5Zm12.621-9.44c-1.409 1.41-4.242 1.061-4.242 1.061s-.349-2.833 1.06-4.242a2.25 2.25 0 0 1 3.182 3.182ZM10.773 7.63c1.409 1.409 1.06 4.242 1.06 4.242S9 12.22 7.592 10.811a2.25 2.25 0 1 1 3.182-3.182Z" />
+</svg>
+`,
+];
 
 renderExpense();
+
+//Expense form button
+const expenseButtonContainer = document.querySelector(".form-button-container");
+const expenseButton = document.querySelectorAll(".form-button");
+
+expenseButtonContainer.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const buttonClicked = e.target.closest(".form-button");
+
+  if (!buttonClicked) return;
+
+  if (buttonClicked.classList.contains("cancel")) {
+    hideModal();
+  }
+
+  if (buttonClicked.classList.contains("add")) {
+    const iconOptions = [
+      "Food and Drinking",
+      "Transportation",
+      "Shopping",
+      "Bill and Utilities",
+      "Entertainnment",
+      "Others",
+    ];
+    const form = e.target.closest(".form");
+    let category = form.category.value;
+    let iconIndex = iconOptions.indexOf(category);
+    let amount = -+form.amount.value;
+    let description = form.description.value;
+    let date = form.date.value;
+
+    if (!category || !amount || !description || !date)
+      throw new Error("INVALID INPUT");
+
+    expenses.push({
+      icon: iconIndex,
+      amount: amount,
+      category: category,
+      description: description,
+      date: date,
+    });
+
+    console.log(expenses);
+    tableBody.innerHTML = "";
+    renderExpense();
+    updateDashboard();
+    form.category.value =
+      form.amount.value =
+      form.description.value =
+      form.date.value =
+        "";
+    hideModal();
+  }
+});
